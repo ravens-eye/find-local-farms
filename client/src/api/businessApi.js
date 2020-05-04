5daa51cb0a229f2809143eb332d1ef5e849852cf
Karsten Rabe <karstenrabe91@gmail.com>
2020-05-03 20:03:52 -0400

    Feat(client-api): Abstract api service and response config

---
+++ client/src/api/businessApi.js
@@ -0,0 +1,12 @@
+
+import { asyncGet } from '../utils/axiosConfig';
+
+export async function getAllBusinesses () {
+  return asyncGet('/api/business');
+}
+
+const businessApi = {
+  getAllBusinesses: getAllBusinesses
+};
+
+export default businessApi;
