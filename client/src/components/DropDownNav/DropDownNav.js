3aeba59137d010f58ceeb0834fb62b8ea6e4913d
Jon Jackson <ocskier@gmail.com>
2020-05-18 18:00:20 -0400

    working on linting

--- client/src/components/DropDownNav/DropDownNav.js
+++ client/src/components/DropDownNav/DropDownNav.js
@@ -3,15 +3,15 @@ import Button from '@material-ui/core/Button';
 import Menu from '@material-ui/core/Menu';
 import MenuItem from '@material-ui/core/MenuItem';
 import { Link } from 'react-router-dom';
-import './DropDownNav.css'
+import './DropDownNav.css';
 import MenuIcon from '@material-ui/icons/Menu';
- 
+
 class SimpleMenu extends React.Component {
   state = {
     anchorEl: null,
   };
 
-  handleClick = event => {
+  handleClick = (event) => {
     this.setState({ anchorEl: event.currentTarget });
   };
 
@@ -24,24 +24,20 @@ class SimpleMenu extends React.Component {
 
     return (
       <div>
-        <Button
-          aria-owns={anchorEl ? 'simple-menu' : undefined}
-          aria-haspopup="true"
-          onClick={this.handleClick}
-        >         
-          <MenuIcon style={{height: '40px', width: '40px'}} />         
+        <Button aria-owns={anchorEl ? 'simple-menu' : undefined} aria-haspopup="true" onClick={this.handleClick}>
+          <MenuIcon style={{ height: '40px', width: '40px' }} />
         </Button>
-        <Menu
-          id="simple-menu"
-          anchorEl={anchorEl}
-          open={Boolean(anchorEl)}
-          onClose={this.handleClose}
-        >
-          <MenuItem className="menuItem" onClick={this.handleClose} component={Link} to="/">Home</MenuItem>
+        <Menu id="simple-menu" anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={this.handleClose}>
+          <MenuItem className="menuItem" onClick={this.handleClose} component={Link} to="/">
+            Home
+          </MenuItem>
           {/* <MenuItem className="menuItem" onClick={this.handleClose} component={Link} to="/admin">admin</MenuItem> */}
-          <MenuItem className="menuItem" onClick={this.handleClose} component={Link} to="/admin">Admin</MenuItem>
-
-          <Button id="menuItem" target="_top"><b>Log In</b></Button> 
+          <MenuItem className="menuItem" onClick={this.handleClose} component={Link} to="/admin">
+            Admin
+          </MenuItem>
+          <Button id="menuItem" target="_top">
+            <b>Log In</b>
+          </Button>
         </Menu>
       </div>
     );
